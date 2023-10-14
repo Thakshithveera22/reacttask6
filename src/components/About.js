34cedb41caad9017e7cf2a8dbd5c592815c47662
Thakshithveera22 <thakshithneerukonda22@gmail.com>
2023-10-14 22:45:22 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+// AboutMe.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+      Welcome to my portfolio website. I am a dedicated Computer Science and Engineering Core (CSE) student currently in my third year of B.Tech studies. Over the past years, I've been honing my skills and exploring the fascinating world of technology. My journey in CSE has equipped me with a strong foundation in various programming languages, data structures, and algorithms, Theory of Computation, Compiler Design. I'm passionate about leveraging technology to solve real-world problems and am enthusiastic to share my experiences and projects with you
+      
+      </p>
+      
+    </section>
+  );
+};
+
+export default About;
+
---
+++ src/components/Clubs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Clubs = () => {
+  return (
+    <section>
+            <h2>Clubs & Organizations</h2>
+            <ul>
+                <li>Sahithi - Member(Management) - 2021</li>
+                <li>Sahiti (Cultural) - Member - 2021</li>
+                <li> Worked with the team members and done many events in the club i.e Telugu Sahithi Literary Association.</li>
+            </ul>
+    </section>
+  );
+}
+
+export default Clubs;
+
